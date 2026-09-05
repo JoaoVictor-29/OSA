97a7a93de15ec2e1adfd27187c290b16c8b277d5
JoaoVictor-29 <jvrp1000@gmail.com>
2026-09-05 18:07:07 -0300

    Arquivos

---
+++ TrabalhoPrático1/aluno.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+class Aluno{
+    
+private:
+    int matricula;
+    string nome;
+    int idade;
+    string curso;
+    string cidade;
+    string uf;
+    float cra;
+
+public:
+
+};
---
+++ TrabalhoPrático1/aluno.h
@@ -0,0 +1,28 @@
+#ifndef ALUNO_H
+#define ALUNO_H
+
+using namespace std;
+
+class Aluno{
+    
+private:
+    int matricula;
+    string nome;
+    int idade;
+    string curso;
+    string cidade;
+    string uf;
+    float cra;
+
+public:
+
+    int packFixo(char* buffer);
+    string packDelimitado();
+    int packDelimitado(char* buffer);
+    int packIdicador(char* buffer);
+    void unpackFixo(const char* buffer);
+    void unpackDelimitado(const string& buffer);
+    void unpackIndicador(const char* buffer);
+    int getBytesUteis();
+    void imprimir();
+};

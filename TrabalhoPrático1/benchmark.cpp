8ee04351c29a62b7e495e5994b71de95c65a4146
kaylane17 <kaylanelelis7@gmail.com>
2026-09-13 11:49:25 -0300

    benchmark.cpp

--- TrabalhoPrático1/benchmark.cpp
+++ TrabalhoPrático1/benchmark.cpp
@@ -9,12 +9,40 @@ using namespace std;
 
 void Benchmark::executar(string caminhoCSV){
     vector<Aluno> alunos = GerenciadorArquivo::lerCSV(caminhoCSV);
-    auto inicio = chrono::high_resolution_clock::now();
+    if(alunos.empty()){
+        cout << "Erro: Arquivo CSV vazio ou não enconrado! \n";
+        return;
+    }
+    Metricas met;
+    met.quantidadeRegistros = alunos.size();
+    string arqFixo = "alunos_fixo.bin";
+    string arqDelimitado = "alunos_delimitado.bin";
+    string arqIndicador = "alunos_indicador.bin";
 
-    GerenciadorArquivo::salvarFixo(alunos_fixo.bin,alunos);
-    auto fim = chrono::high_resolution_clock::now();
-    chrono::duration<double, milli> duracao = fim - inicio;
-    cout << "Tempo de execução: " << duracao.count() << "ms\n";
+    using clock = chrono::high_resolution_clock;
+    using ms = chrono::duration<double, milli>;
 
+    auto inicio = clock::now();
+    GerenciadorArquivo::salvarFixo(arqFixo, alunos);
+    auto fim = clock::now();
+    met.tempoEscritaFixo = ms(fim - inicio).count();
 
+    inicio =clock::now();
+    GerenciadorArquivo::salvarDelimitado(arqDelimitado, alunos);
+    fim = clock::now();
+    met.tempoEscritaDelimitado = ms(fim - inicio).count();
+
+    inicio = clock::now();
+    GerenciadorArquivo::salvarDelimitado(arqIndicador, alunos);
+    fim = clock::now();
+    met.tempoEscritaIndicador = ms(fim - inicio).count();
+
+    inicio = clock::now();
+    vector<Aluno> lidosFixo = GerenciadorArquivo::lerFixo(arqFixo);
+    fim = clock::now();
+    met.tempoLeituraFixo = ms(fim - inicio).count();
+
+    inicio = clock::now();
+    vector<Aluno> lidosDelimitado = GerenciadorArquivo::lerDelimitado(arqDelimitado);
+    fim = clock::now();
 }
